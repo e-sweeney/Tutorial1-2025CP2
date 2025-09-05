f686dbe987c1e6c6a10d39b6caf0f0203a6bae67
e-sweeney <edwina.sweeney@atu.ie>
2025-09-05 12:16:09 +0100

    Added few lines to Week1Tutorial1.cs

---
+++ Week1Tutorial1.cs
@@ -0,0 +1,24 @@
+using System;
+Console.WriteLine("This is a simple C# program.");
+Method01();
+Method02();
+Method03();
+
+System.Console.WriteLine("Main program completed.");
+
+
+static void Method01()
+{
+    System.Console.WriteLine("Method01 called");
+}
+
+static void Method02()
+{
+    System.Console.WriteLine("Method02 called");
+}   
+
+static void Method03()
+{
+    System.Console.WriteLine("Method03 called");
+}
+
